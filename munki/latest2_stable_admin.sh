02370542541219770bed806d121e23ae3d3f7295
unknown <nlfits-admin@NLFDSS.main.ad.rit.edu>
2015-04-22 17:05:05 -0400

    Adding script to pull latest stable build of munki from offical github releases.

---
+++ munki/latest2_stable_admin.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# Download and install the latest Munki2 tools from munkibuilds.org
+
+cat <<EOF
+
+**************************
+** Munki auto-installer **
+**************************
+
+EOF
+if [[ $EUID -ne 0 ]]; then
+    echo "(Please enter your sudo password when prompted)"
+    echo ""
+fi
+
+tmpdir=$(mktemp -d /tmp/munkibuilds-XXXX)
+pkg_download="${tmpdir}/munki2.pkg"
+choices_download="${tmpdir}/admin.xml"
+latest_stable_release=$(curl -s https://api.github.com/repos/munki/munki/releases | python -c 'import json,sys;obj=json.load(sys.stdin);print obj[0]["assets"][0]["browser_download_url"]')
+echo "Grabbing the latest version..."
+curl \
+	-s \
+	-L \
+    -o "${pkg_download}" \
+    --connect-timeout 30 \
+    "${latest_stable_release}"
+	
+curl \
+    -s \
+    -o "${choices_download}" \
+    --connect-timeout 30 \
+    https://raw.githubusercontent.com/n8felton/Mac-OS-X-Scripts/master/munki/admin.xml
+
+
+sudo /usr/sbin/installer -applyChoiceChangesXML "${choices_download}" -pkg "${pkg_download}" -target /
