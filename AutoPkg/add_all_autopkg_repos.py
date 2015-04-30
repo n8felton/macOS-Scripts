d9ecae1533a41f3e35dd3fa4080403f42f71b9b7
unknown <nlfits-admin@NLFDSS.main.ad.rit.edu>
2015-04-30 16:01:26 -0400

    Changing to use 'clone_url' vs 'html_url' to fix duplicates issue with AutoPkgr

--- AutoPkg/add_all_autopkg_repos.py
+++ AutoPkg/add_all_autopkg_repos.py
@@ -8,6 +8,6 @@ ORG_REPOS_URL = "https://api.github.com/orgs/autopkg/repos"
 org_repos = urllib2.urlopen(ORG_REPOS_URL)
 org_repos_data = json.load(org_repos)
 for repo in org_repos_data:
-    print repo['html_url']
-    subprocess.call(['autopkg', 'repo-add', repo['html_url']])
+    print repo['clone_url']
+    subprocess.call(['autopkg', 'repo-add', repo['clone_url']])
 
