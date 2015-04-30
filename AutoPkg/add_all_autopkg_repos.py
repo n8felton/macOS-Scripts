e8630eea10d420ff5277b0019557743850310761
unknown <nlfits-admin@NLFDSS.main.ad.rit.edu>
2015-04-30 16:10:03 -0400

    Making sure we get all the repos.

--- AutoPkg/add_all_autopkg_repos.py
+++ AutoPkg/add_all_autopkg_repos.py
@@ -3,7 +3,7 @@ import json
 import urllib2
 import subprocess
 
-ORG_REPOS_URL = "https://api.github.com/orgs/autopkg/repos"
+ORG_REPOS_URL = "https://api.github.com/orgs/autopkg/repos?per_page=100"
 
 org_repos = urllib2.urlopen(ORG_REPOS_URL)
 org_repos_data = json.load(org_repos)
