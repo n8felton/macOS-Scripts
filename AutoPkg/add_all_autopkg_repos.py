03de69b47d2a14d5d709d578539fac8d4385a2dc
n8felton <n8felton@gmail.com>
2015-07-21 12:51:15 -0400

    Adding support to import in order based on repo stars

--- AutoPkg/add_all_autopkg_repos.py
+++ AutoPkg/add_all_autopkg_repos.py
@@ -2,12 +2,12 @@
 import json
 import urllib2
 import subprocess
+from operator import itemgetter
 
 ORG_REPOS_URL = "https://api.github.com/orgs/autopkg/repos?per_page=100"
 
 org_repos = urllib2.urlopen(ORG_REPOS_URL)
 org_repos_data = json.load(org_repos)
-for repo in org_repos_data:
+for repo in sorted(org_repos_data, key=itemgetter('stargazers_count'), reverse=True):
     print repo['clone_url']
     subprocess.call(['autopkg', 'repo-add', repo['clone_url']])
-
