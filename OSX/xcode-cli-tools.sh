0c93bbb6ce8dc8cbdef45d46a82d9ffc4be9aa34
n8felton <n8felton@gmail.com>
2016-04-19 10:43:01 -0400

    Adding fork of xcode-cli-tools

---
+++ OSX/xcode-cli-tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Origainally from https://github.com/timsutton/osx-vm-templates/blob/8a2d0be3b8c3d3ba81aa49e7930a2d65ed7e70fc/scripts/xcode-cli-tools.sh
+# Thanks to Tim Sutton
+
+# create the placeholder file that's checked by CLI updates .dist code
+# in Apple's SUS catalog
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+# find the CLI Tools update
+PROD=$(softwareupdate -l | awk -F"*" '/\* Command Line Tools/ {gsub(/^[[:space:]]/,"",$2); print $2}')
+# install it
+softwareupdate -i "${PROD}" -v
+# cleanup the placeholder
+rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
